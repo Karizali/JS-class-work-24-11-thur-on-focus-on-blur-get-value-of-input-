2feba4753ccd39b7beb128c73ee3aaaa3ed329f0
Karizali <96954751+Karizali@users.noreply.github.com>
2022-11-24 20:55:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+// function abc (a){
+//     var a=0;
+//     console.log(a); 
+
+// }
+
+// function xyz (d){
+    
+//     console.log(d); 
+
+
+
+// }
+// get value of input
+// var valget = document.getElementById("get")
+// function val(){
+    
+//     console.log(valget.value);
+    
+// }
+
+
+// value push to an arry
+
+// var valget = document.getElementById("get")
+
+// var arr = []
+// function run(){
+
+//     arr.push(valget.value)
+//     console.log(arr);
+// }
+
+// If input does not input by the user
+
+
+// function erroring() {
+//     var p = document.getElementById("hello");
+//     var g = document.getElementById("error");
+//     var f=[]
+
+//     var valOfp= p.value;
+//     if(valOfp = " "){
+//         g.value = "Enter the value"
+//     }else{
+//         f.push(p.value);
+//         console.log(f);
+//     }
+// }
+
